bea0735a3d0185ea7630c346e037900a29d39793
thr <johannes.thraen@gmail.com>
2016-02-15 19:37:56 +0100

    differenz zur vorgabe eingebaut

--- test.jl
+++ test.jl
@@ -1,57 +1,20 @@
-#rmprocs( procs()[2:end] )
-#addprocs(1)
-#addprocs(5)
-
-# @everywhere const m					= 256+4
-# @everywhere const n					= 256+4
-# @everywhere const m					= 140
-# @everywhere const n					= 140
 @everywhere const m					= 60
 @everywhere const n					= 65
 
-#@everywhere const m					= 30
-#@everywhere const n					= 30
-
 # fuer die Konstruktion der Zeitregularisierungsmatrizen muss n_samples >=2 und n_zwischensamples >=3 sein!
 @everywhere const n_samples			= 5
-#@everywhere const n_samples			= 2
-#@everywhere const n_zwischensamples	= 2    # duerfen nicht zu wenige sein? abhaengig von dt?
-@everywhere const n_zwischensamples	= 9    # duerfen nicht zu wenige sein? abhaengig von dt?
-#@everywhere const n_zwischensamples	= 29    # duerfen nicht zu wenige sein? abhaengig von dt?
-#@everywhere const n_zwischensamples	= 39    # duerfen nicht zu wenige sein? abhaengig von dt?
 
+# @everywhere const n_zwischensamples	= 9    # duerfen nicht zu wenige sein? abhaengig von dt?
 # ...................... T, alle ZeitPUNKTE, also T-1 Zeitschritte von einem Punkt auf den naechsten
-@everywhere const T					= (n_samples-1)*(n_zwischensamples+1) +1
-@show T
+#@everywhere const T					= (n_samples-1)*(n_zwischensamples+1) +1
 # Zuordnung Samplenummer zu Zeitpunkt 
-@show sample_times		= [ (k+1, k*(n_zwischensamples+1)+1) for k in 0:n_samples-1 ]
-
-# what, das ist doch falsch!
-# @everywhere const T					= (n_samples-1)*n_zwischensamples+1
-#sample_times		= [ (k+1, k*n_zwischensamples+1) for k in 0:n_samples-1 ]
+#sample_times						= [ (k+1, k*(n_zwischensamples+1)+1) for k in 0:n_samples-1 ]
 
 armijo_bas			= 0.5
 armijo_sig			= 0.0
 
-#@everywhere const dt			= 1/T
-#@everywhere const dx			= 1/m
-
 @everywhere const dt			= 0.01
-# @everywhere const dt			= 0.04
 @everywhere const dx			= 0.01
-
-# gut bei every = 3,4,5 da funktionierern hoehere faktoren nicht mehr
-# @everywhere const alpha	= 0.1 
-# @everywhere const beta	= 0.08  
-
-#@everywhere const alpha	= 0.001/4/1.38916667/1.399
-#@everywhere const beta	= 0.001/4/1.38916667/1.399
-#step	455	39	test armijo step length 	1.8189894035458565e-12	
-#rL2errors 	0.0001653830487966356	0.00016538304879663488	7.316901284020345e-19	
-#L2errors 	2.2974461861999313e-5	2.297446186199921e-5	1.0164395367051604e-19	
-#H1_errors	5.371933970456542e-5	5.371933970456554e-5	-1.2197274440461925e-19	
-#J        	3.8346900783282365e-5	3.834690078328238e-5	-1.3552527156068805e-20	
-
 
 @everywhere const alpha	= 0.001 #*0.001 #warum ist das nicht dasselbe, wie die norm noch mal mit alpha zu multiplizieren? siehe CostNormOp --> thr ahh, wegen der ruecksubstitution nach ellipt gleichung. aber warum funktioniert es so gut, wenn man die norm noch mal mit alpha multipliziert. teste das auch mal ohne zeitreg
 @everywhere const beta	= 0.001
@@ -98,24 +61,30 @@ timereg_solver	= "multig"#fur gegebene Probleme am besten
 # velocities_at		= ~time_regularization ? velocities_at : "centers"
 
 include("view.jl")
-#pygui(true)
+pygui(true)
 include("beispiele.jl")
 
+@everywhere const auslassen				= 2 # die Referenzsamples werden so gewählt, dass aus der Vorgabe werden immer `auslassen` Frames weggelassen werden
+@everywhere const zwischen_ausgelassen	= 2 # zwischen zwei ausgelassenen Frames sollen so viele Zwischenframes generiert werden.
+# die Anzahl zwischen den Referenzframes zu generierenden Frames. 
+@everywhere const n_zwischensamples		= auslassen + (auslassen+1) * zwischen_ausgelassen
+@everywhere const sample_times			= [ (k+1, k*(n_zwischensamples+1)+1) for k in 0:n_samples-1 ]
 
-#every = 10
-#every = 4
-#every = 2
-#@show vorgabe_sample_times = (1:every:every*n_samples) 
-#@show T_vorgabe	= vorgabe_sample_times[end]
+@everywhere const T						= (n_samples-1)*(n_zwischensamples+1) +1
 
-#I_given = init_vorgabe(char_quadrat, m,n, T_vorgabe) 	###aaaahhrg
-#s		= I_given[:,:,vorgabe_sample_times] 		###aaaahhrg
- s		= inits(quadrat)
+@everywhere const vorgabe_used_indices	= (1:(auslassen+1):(auslassen+1)*n_samples) 
+@everywhere const T_vorgabe				= vorgabe_used_indices[end]
+@everywhere const samples_to_vorgabe	= [(k, vorgabe_used_indices[k]) for k in 1:n_samples]
 
-# s		= inits(rot_circle)
-# s		= inits(rot_circle_ex)[:,:,1:n_samples]
-# s		= load_taxi(m,n,41)[:,:, 1:5:end]
-# s		= readtaxi_alt()[:,:, 1:5:5*n_samples+1]
+@everywhere const vorgabe_frames		= (1:(zwischen_ausgelassen+1):(zwischen_ausgelassen+1)*T_vorgabe) 
+@everywhere const vorgabe_to_frames		= [(k,vorgabe_frames[k]) for k in 1:T_vorgabe]
+
+
+# Zuordnung Samplenummer zu Zeitpunkt 
+
+I_given = init_vorgabe(char_quadrat, m,n, T_vorgabe) 	###aaaahhrg
+s		= I_given[:,:,vorgabe_used_indices] 		###aaaahhrg
+# s		= inits(quadrat)
 
 velocities_at == "centers" && begin
 	u		= 0* ones( m, n, T-1 )
@@ -146,5 +115,13 @@ steps=1
 
 @time I, u, v, p, L2_err, H1_err, J, H1_J_w, steps = verfahren_grad(s, u, v, steps)
 #@time I, u, v, p, L2_err, H1_err, J, H1_J_w, steps = verfahren_grad_altnormalization(s, u, v, steps)
+
+# Differenz zur Vorgabe
+
+diff_vorgabe	= zeros( size(I_given) )
+for t in 1:T_vorgabe
+	@show j				= vorgabe_frames[t]
+	diff_vorgabe[:,:,t]	= I_given[:,:,t] - I[:,:,j]
+end
 
 _="fertig"
